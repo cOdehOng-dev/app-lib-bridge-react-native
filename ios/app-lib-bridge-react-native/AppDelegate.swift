8ceca05e36dad58b3435898674f884447ff815f8
오스카(홍석우)_APP개발2 <ghdtjrdn0323@nol-universe.com>
2026-04-27 11:10:34 +0900

    - initial commit

--- ios/app-lib-bridge-react-native/AppDelegate.swift
+++ ios/app-lib-bridge-react-native/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "BridgeLib",
+      withModuleName: "app-lib-bridge-react-native",
       in: window,
       launchOptions: launchOptions
     )
