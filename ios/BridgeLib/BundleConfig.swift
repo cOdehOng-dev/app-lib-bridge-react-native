74fff696b419beea7e23c67f4f3ecda48edd0f4d
오스카(홍석우)_APP개발2 <ghdtjrdn0323@nol-universe.com>
2026-04-29 08:42:11 +0900

    feat: add iOS BundleConfig

---
+++ ios/BridgeLib/BundleConfig.swift
@@ -0,0 +1,33 @@
+import Foundation
+import React
+
+@objc public class BundleConfig: NSObject {
+    @objc public let devURL: URL
+    @objc public let assetName: String
+    @objc public let localBundleURL: URL?
+
+    /// - Parameters:
+    ///   - devURL: Metro 개발 서버 URL (DEBUG 빌드에서 사용)
+    ///   - assetName: 앱 번들 내 .jsbundle 파일명 (확장자 제외, 예: "main")
+    ///   - localBundleURL: OTA로 다운로드된 번들의 로컬 파일 URL. nil이면 assetName 사용
+    @objc public init(
+        devURL: URL,
+        assetName: String,
+        localBundleURL: URL? = nil
+    ) {
+        self.devURL = devURL
+        self.assetName = assetName
+        self.localBundleURL = localBundleURL
+    }
+
+    func resolvedURL() -> URL? {
+        #if DEBUG
+        return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+        #else
+        if let localURL = localBundleURL {
+            return localURL
+        }
+        return Bundle.main.url(forResource: assetName, withExtension: "jsbundle")
+        #endif
+    }
+}
